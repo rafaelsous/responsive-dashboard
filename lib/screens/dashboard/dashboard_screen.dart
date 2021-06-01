fc15231eee6aab7bcfc2614cc5681cfa8a914687
Rafael Lima de Sousa <rafael.desousa@roberthalfprojects.com>
2021-06-01 16:30:10 -0300

    Created storage info card component.

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'components/header.dart';
 import 'components/chart.dart';
+import 'components/storage_info_card.dart';
 
 import 'package:admin/constants.dart';
 
@@ -16,25 +17,29 @@ class DashboardScreen extends StatelessWidget {
             Header(),
             SizedBox(height: defaultPadding),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
                   flex: 5,
                   child: Container(
                     height: 500.0,
-                    color: Colors.pink,
+                    decoration: BoxDecoration(
+                      color: Colors.pink,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ),
                   ),
                 ),
                 SizedBox(width: defaultPadding),
                 Expanded(
                   flex: 2,
                   child: Container(
-                    height: 500.0,
                     padding: EdgeInsets.all(defaultPadding),
                     decoration: BoxDecoration(
                       color: secondaryColor,
                       borderRadius: const BorderRadius.all(Radius.circular(10)),
                     ),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           "Storage Details",
@@ -45,6 +50,30 @@ class DashboardScreen extends StatelessWidget {
                         ),
                         SizedBox(height: defaultPadding),
                         Chart(),
+                        StorageInfoCard(
+                          svgSource: "assets/icons/Documents.svg",
+                          title: "Documents Files",
+                          numOfFiles: 1876,
+                          amountOfFiles: "1.45Gb",
+                        ),
+                        StorageInfoCard(
+                          svgSource: "assets/icons/media.svg",
+                          title: "Media Files",
+                          numOfFiles: 297,
+                          amountOfFiles: "6.91Gb",
+                        ),
+                        StorageInfoCard(
+                          svgSource: "assets/icons/folder.svg",
+                          title: "Others Files",
+                          numOfFiles: 7239,
+                          amountOfFiles: "0.85Gb",
+                        ),
+                        StorageInfoCard(
+                          svgSource: "assets/icons/unknown.svg",
+                          title: "Unknown",
+                          numOfFiles: 213,
+                          amountOfFiles: "0.17Gb",
+                        ),
                       ],
                     ),
                   ),
