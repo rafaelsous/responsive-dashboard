9274f7dac74fd36914ac5cac894b4474d7d7d882
Rafael Lima de Sousa <rafael.desousa@roberthalfprojects.com>
2021-06-02 10:46:47 -0300

    Created recent_files component.

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'components/header.dart';
 import 'components/my_files.dart';
+import 'components/recent_files.dart';
 import 'components/storage_details.dart';
 
 import 'package:admin/constants.dart';
@@ -21,7 +22,13 @@ class DashboardScreen extends StatelessWidget {
               children: [
                 Expanded(
                   flex: 5,
-                  child: MyFiles(),
+                  child: Column(
+                    children: [
+                      MyFiles(),
+                      SizedBox(height: defaultPadding),
+                      RecentFiles(),
+                    ],
+                  ),
                 ),
                 SizedBox(width: defaultPadding),
                 Expanded(
